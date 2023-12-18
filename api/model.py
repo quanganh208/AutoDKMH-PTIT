83a6e0cfbfee9515532281bb433f8335daed4943
quanganh.208 <nquanganh478@gmail.com>
2023-12-18 12:21:37 +0700

    test: v1.0.0

---
+++ api/model.py
@@ -0,0 +1,8 @@
+class Course:
+    def __init__(self, id_to_hoc, idCourse, nameCourse, group, team, className):
+        self.id_to_hoc = id_to_hoc
+        self.idCourse = idCourse
+        self.nameCourse = nameCourse
+        self.group = group
+        self.team = team
+        self.className = className
