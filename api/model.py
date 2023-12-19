078127fd4c3060bf4fd72e8a97068c0ff0293278
quanganh.208 <nquanganh478@gmail.com>
2023-12-19 08:48:21 +0700

    releases: v1.0.0

--- api/model.py
+++
@@ -1,9 +0,0 @@
-class Course:
-    def __init__(self, id_to_hoc, idCourse, nameCourse, group, team, className, remainingAmount):
-        self.id_to_hoc = id_to_hoc
-        self.idCourse = idCourse
-        self.nameCourse = nameCourse
-        self.group = group
-        self.team = team
-        self.className = className
-        self.remainingAmount = remainingAmount
